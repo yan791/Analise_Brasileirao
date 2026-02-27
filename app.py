640f4f9e4c952f14a3d38558e302c5f7db56fc2f
Yan Ribeiro Nunes <yrn@cesar.school>
2026-02-27 16:35:44 -0300

    Adicionando a 1 funcao do app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st 
 import pandas as pd
 
-st.title("Dashboard do Brasileirão (2003 - 2024)")
-st.write("Análise de Times Brasileirão")
+st.title("Dashboard do Brasileirão")
+st.write("Análise de Times Brasileirão (2003 - 2024)")
 
 dados = pd.read_csv('brasileirao.csv')
 
@@ -12,3 +12,5 @@ opcao = st.selectbox(
 )
 if opcao == "1. Maiores Campeões (1º Lugar)":
     df_campeoes = dados[dados['place'] == 1]
+    contagem = df_campeoes['team'].value_counts()
+    st.bar_chart(contagem)
