6dcffa71878c71a39c2f68a9531336ed752ac029
Caio Leimig <caiolrs2005@gmail.com>
2026-02-27 17:56:59 -0300

    adicionando times com mais gols e mais vitorias

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Dashboard do Brasileirão")
 st.write("Análise de Times Brasileirão (2003 - 2024)")
@@ -8,9 +9,18 @@ dados = pd.read_csv('brasileirao.csv')
 
 opcao = st.selectbox(
     "📊 Escolha qual estatística você quer analisar:",
-    ("1. Maiores Campeões (1º Lugar)", "2. Times com Mais Gols", "3. Times com Mais Vitórias")
+    ("1. Maiores Campeões", "2. Times com Mais Gols", "3. Times com Mais Vitórias", "4. Media de Gols por Temporada")
 )
-if opcao == "1. Maiores Campeões (1º Lugar)":
+
+if opcao == "1. Maiores Campeões":
     df_campeoes = dados[dados['place'] == 1]
     contagem = df_campeoes['team'].value_counts()
     st.bar_chart(contagem)
+
+elif opcao == "2. Times com Mais Gols":
+    totalgol = dados.groupby('team')['goals'].sum().sort_values(ascending = False)
+    st.bar_chart(totalgol.head(10))
+
+elif opcao == "3. Times com Mais Vitórias":
+    maisvit = dados.groupby('team')['won'].sum().sort_values(ascending= False)
+    st.bar_chart(maisvit.head(10))
